ff4b9005fd248ffafbdf84d5bf490d5905eb1173
jmattefs <josephmattefs@gmail.com>
2016-03-24 14:35:01 -0500

    paypal start

--- FinalProject/FinalProject/Controllers/EmployersController.cs
+++ FinalProject/FinalProject/Controllers/EmployersController.cs
@@ -7,117 +7,110 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using FinalProject.Models;
-using Microsoft.AspNet.Identity;
 
 namespace FinalProject.Controllers
 {
-    public class JobSeekersController : Controller
+    public class EmployersController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: JobSeekers
+        // GET: Employers
         public ActionResult Index()
         {
-            return View(db.JobSeeker.ToList());
+            return View(db.Employer.ToList());
         }
 
-        [AllowAnonymous]
-        // GET: JobSeekers/Details/5
+        // GET: Employers/Details/5
         public ActionResult Details(int? id)
         {
-            var UserID = User.Identity.GetUserId();
-            var user = db.Users.Where(x => x.Id == UserID).Select(x => x).FirstOrDefault();
-            JobSeeker js = new JobSeeker();
-            js = db.JobSeeker.Where(x => x.UserId == user.Id).Select(x => x).FirstOrDefault();
-
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            //JobSeeker jobSeeker = db.JobSeeker.Find(id);
-            //if (jobSeeker == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            return View(js);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employer employer = db.Employer.Find(id);
+            if (employer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employer);
         }
 
-        // GET: JobSeekers/Create
+        // GET: Employers/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: JobSeekers/Create
+        // POST: Employers/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Address,City,State,ZipCode,Info,")] JobSeeker jobSeeker)
+        public ActionResult Create([Bind(Include = "ID,Name,Address,City,State,ZipCode,Info,isSubscribed")] Employer employer)
         {
             if (ModelState.IsValid)
             {
-                db.JobSeeker.Add(jobSeeker);
+                db.Employer.Add(employer);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(jobSeeker);
+            return View(employer);
         }
 
-        // GET: JobSeekers/Edit/5
+        // GET: Employers/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            JobSeeker jobSeeker = db.JobSeeker.Find(id);
-            if (jobSeeker == null)
+            Employer employer = db.Employer.Find(id);
+            if (employer == null)
             {
                 return HttpNotFound();
             }
-            return View(jobSeeker);
+            return View(employer);
         }
 
-        // POST: JobSeekers/Edit/5
+        // POST: Employers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Address,City,State,ZipCode,Info")] JobSeeker jobSeeker)
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,City,State,ZipCode,Info,isSubscribed")] Employer employer)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(jobSeeker).State = EntityState.Modified;
+                db.Entry(employer).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(jobSeeker);
+            return View(employer);
         }
 
-        // GET: JobSeekers/Delete/5
+        // GET: Employers/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            JobSeeker jobSeeker = db.JobSeeker.Find(id);
-            if (jobSeeker == null)
+            Employer employer = db.Employer.Find(id);
+            if (employer == null)
             {
                 return HttpNotFound();
             }
-            return View(jobSeeker);
+            return View(employer);
         }
 
-        // POST: JobSeekers/Delete/5
+        // POST: Employers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            JobSeeker jobSeeker = db.JobSeeker.Find(id);
-            db.JobSeeker.Remove(jobSeeker);
+            Employer employer = db.Employer.Find(id);
+            db.Employer.Remove(employer);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
